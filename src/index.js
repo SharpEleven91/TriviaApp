6492208c62644ff670e504666f77da93e9e3bba9
Cody Biller <sharpeleven91@gmail.com>
2017-09-12 17:44:27 -0400

    add api calls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import './index.scss';
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const api = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
+class Fetch extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            questions: [],
+            answers: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
+        .then( data => {
+            this.setState({questions: data.data.results[0].question, answers: data.data.results[0].correct_answer})
+            console.log(data);
+        })
+    }
+
+    render() {
+        var style = {
+            padding: "20px 20px 20px 20px"
+        }
+        return (
+            <div style = {style}>
+                <p>{this.state.questions}</p>
+                <p>{this.state.answers}</p>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <div> <Fetch/> </div>,
+    document.getElementById("triviaBox")
+)
