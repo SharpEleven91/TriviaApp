9e719a3edb189c1d1f868548268f9509ae24844a
Cody Biller <sharpeleven91@gmail.com>
2017-09-12 16:28:36 -0400

    set up base styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './index.scss';
+import react from 'react';
+import ReactDOM from 'react-dom';
 
+const api = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
