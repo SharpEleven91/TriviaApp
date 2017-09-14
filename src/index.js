09b68c9d4a71e20980dabaac35743927415004f8
Cody Biller <sharpeleven91@gmail.com>
2017-09-13 23:04:50 -0400

    refactor get requests

--- src/index.js
+++ src/index.js
@@ -6,29 +6,41 @@ import jquery from 'jquery';
 const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
 
+class GetQuestion extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            questions: [],
+            answers: [],
+            correctAnswer: [] 
+        }
+    }
+    
+    componentWillMount() {
+        axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
+        .then(data => {
+            this.setState({questions: data.data.results[0].question,
+            correctAnswer: data.data.results[0].correct_answer, answers: data.data.results[0].incorrect_answers})
+        });
+    }
+    render() {
+        return( <Fetch questions = {this.state.questions} correctAnswer = {this.state.correctAnswer} answers = {this.state.answers}/>
+        )
+    }
+}
 class Fetch extends React.Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this)
         this.state = {
-            questions: [],
-            answers: [],
-            correctAnswer: [],
             score: 0,
-            rounds: 0,
+            rounds: 0
         };
-
-    }
-    componentDidMount() {
-        axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
-        .then(data => {
-            this.setState({questions: data.data.results[0].question,
-                 correctAnswer: data.data.results[0].correct_answer, answers: data.data.results[0].incorrect_answers})
-        })
+        console.log(this.props)
     }
 
     handleClick(answer) {
-        var correct = this.state.correctAnswer;
+        var correct = this.props.correctAnswer;
         if (answer == correct) {
             this.setState((prevState, props) => {
                 return {score: prevState.score + 1}
@@ -39,15 +51,15 @@ class Fetch extends React.Component {
         this.setState((prevState, props) => {
             return {rounds: prevState.rounds + 1};
         });
-        if (this.state.rounds != 10) {
-            ReactDOM.render(
-                <Fetch/>,
-                document.getElementById("triviaBox")
-            )
-        } else {
+        if (this.state.rounds == 10) {
             ReactDOM.render(
                 <div> You've scored {this.state.score} out of 10!</div>,
                 document.getElementById("triviaBox")
+            )
+        } else {
+            console.log("ay")
+            return (
+                <GetQuestion/>
             )
         }
     }
@@ -58,26 +70,25 @@ class Fetch extends React.Component {
         }
         var answers = [];
 
-        for (let i = 0; i < this.state.answers.length; i++) {
-            answers.push(entities.decode(this.state.answers[i]));
+        for (let i = 0; i < this.props.answers.length; i++) {
+            answers.push(entities.decode(this.props.answers[i]));
         }
-        answers.push(this.state.correctAnswer);
+        answers.push(this.props.correctAnswer);
 
         // Ensure correct answer wont be the last option every time
         var indexedAnswers = answers.sort(function(a, b) {
             return a.length - b.length;
         });
-        return (<div>
-                <div className="question">{entities.decode(this.state.questions)}</div>
-                <div> </div>
-                <div className="answers"> <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[0])}> {indexedAnswers[0]} </button>
-                                          <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[1])}> {indexedAnswers[1]} </button> 
-                                          <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[2])}> {indexedAnswers[2]} </button> 
-                                          <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[3])}> {indexedAnswers[3]} </button> 
+        return  <div>
+                <div className="question">{entities.decode(this.props.questions)}</div>
+                    <div> </div>
+                    <div className="answers"> <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[0])}> {indexedAnswers[0]} </button>
+                                            <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[1])}> {indexedAnswers[1]} </button> 
+                                            <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[2])}> {indexedAnswers[2]} </button> 
+                                            <button className="answerButton" onClick = { () => this.handleClick(indexedAnswers[3])}> {indexedAnswers[3]} </button> 
+                    </div>
                 </div>
-              </div>
-        )
-    }
+        }
 }
 
 class NewGame extends React.Component {
@@ -99,7 +110,7 @@ class NewGame extends React.Component {
                 <button className="newGameButton" onClick = {() => this.handleClick()}> START NEW GAME </button>
                 </div>
         } else {
-            return <Fetch/>
+            return <GetQuestion/>
         }
     }
 }
